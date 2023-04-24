ae2b7100e3709c7df0a1b0a6a35f33ee9d5fb6ba
Kulpot <kulpotwahu@gmail.com>
2023-04-25 06:21:44 +0800

    Add project files.

---
+++ ZippingDirectories/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZippingDirectories
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var dlgFolderBrowser = new FolderBrowserDialog())
+            {
+                if(dlgFolderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    ZipDirToParent(dlgFolderBrowser.SelectedPath);
+                }
+            }
+        }
+        //Zip Method
+        private static void ZipDirToParent(string dir)
+        {
+            string parent = Path.GetDirectoryName(dir);
+            Process.Start(parent);
+            string name = Path.GetFileName(dir);
+            string fileName = Path.Combine(parent, name + ".zip");
+            ZipFile.CreateFromDirectory(dir, fileName, CompressionLevel.Fastest, true);
+
+        }
+    }
+}
